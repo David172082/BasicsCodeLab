7a5eb1e7075e4aa941e4a06c1761ab16f41bb61b
David172082 <david172082@gmail.com>
2025-10-02 23:18:54 -0500

    Animacion lista

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
 
@@ -27,7 +32,14 @@ class MainActivity : ComponentActivity() {
 fun MyApp() {
     var expanded by rememberSaveable { mutableStateOf(false) }
     val itemsList = List(20) { "Elemento #$it" }
-    LazyColumn {
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    LazyColumn(modifier = Modifier.padding(bottom = extraPadding)) {
         items(itemsList) { item ->
             Text(text = item)
         }
