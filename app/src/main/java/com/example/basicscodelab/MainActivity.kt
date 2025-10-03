c4100b4a815a7c679799ff7b8514f4e05eebb167
David172082 <david172082@gmail.com>
2025-10-02 23:16:49 -0500

    Estado persistente

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
@@ -24,7 +25,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    val itemsList = List(1000) { "Elemento #$it" }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val itemsList = List(20) { "Elemento #$it" }
     LazyColumn {
         items(itemsList) { item ->
             Text(text = item)
